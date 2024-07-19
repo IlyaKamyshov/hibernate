14eb6730787b28317d98c332ebd892f6e9019e02
IlyaKamyshov <kamyshov@gmail.com>
2024-07-19 07:32:32 +0300

    Added jpa-repository-query homework

--- src/main/java/ru/netology/hibernate/repository/PersonRepository.java
+++ src/main/java/ru/netology/hibernate/repository/PersonRepository.java
@@ -1,6 +1,7 @@
 package ru.netology.hibernate.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.netology.hibernate.entity.Person;
 import ru.netology.hibernate.entity.PersonPrimaryKey;
@@ -10,7 +11,14 @@ import java.util.Optional;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, PersonPrimaryKey> {
+
+    @Query(value = "SELECT * FROM netology.PERSONS p WHERE p.city_of_living = :city", nativeQuery = true)
     List<Person> findAllByCityOfLiving(String city);
+
+    @Query(value = "SELECT * FROM netology.PERSONS p WHERE p.age < :age ORDER BY p.age", nativeQuery = true)
     List<Person> findAllByIdAgeLessThanOrderByIdAge(short age);
+
+    @Query(value = "SELECT * FROM netology.PERSONS p WHERE p.name = :name AND p.surname = :surname", nativeQuery = true)
     Optional<Person> findAllByIdNameAndIdSurname(String name, String surname);
+
 }
